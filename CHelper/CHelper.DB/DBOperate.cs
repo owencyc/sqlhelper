da88142c63196aee87570a3ebc3ab710bf548eaf
owencyc <owencyc@gmail.com>
2017-01-16 20:58:01 +0800

    1.16

--- CHelper/CHelper.DB/DBOperate.cs
+++ CHelper/CHelper.DB/DBOperate.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +13,18 @@ namespace CHelper.DB
     class DBOperate
     {
         public string ConnectionStringName;
-
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
         public void GetData_Page()
         {
-            
+            Database db = DatabaseFactory.CreateDatabase(GetConnectionString("SQL_String"));
+            DbCommand com = db.GetStoredProcCommand("AddUserInfo");
+            db.AddInParameter(com, "@UserName", DbType.String, "zgshi");
+            db.AddInParameter(com, "@Pwd", DbType.String, "123456");
+            db.ExecuteNonQuery(com);
+
         }
     }
 }
