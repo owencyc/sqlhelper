9a2c91fabe9c67ebb04956d75ca4c38bf3b25bbd
owencyc <owencyc@gmail.com>
2017-01-16 19:59:42 +0800

    project

---
+++ CHelper/CHelper.DB/DBOperate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHelper.DB
+{
+    class DBOperate
+    {
+        public string ConnectionStringName;
+
+        public void GetData_Page()
+        {
+            
+        }
+    }
+}
